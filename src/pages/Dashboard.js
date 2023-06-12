ff44e60af1b9690d1d5a7bf84f33ed308aa00030
Kamal Sagar <kamalsagar316@gmail.com>
2023-06-12 20:34:44 +0530

    Modified dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div className='flex justify-center min-h-screen items-center text-white text-3xl h-full'>
-      Welcome to StudyNotion!!
+      Welcome to QuickLearn!!
     </div>
   )
 }
